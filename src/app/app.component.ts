c2b6f101b78f098744a2cf5bda701d66a3991e59
MatheusLustosa <matheusclc@hotmail.com>
2025-03-28 15:42:17 -0300

    coponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
